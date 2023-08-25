7735734e295b40f80c121099f1f833c8c6b92e2a
Brian Lee <brianlee@Brians-MacBook-Pro-2.local>
2023-08-25 13:39:55 -0700

    buttons

---
+++ script.js
@@ -0,0 +1,30 @@
+//a start button that begins the game
+//letters must match the number of blanks
+//game is timed
+//user wins when all letters of the word has been guessed
+//user loses when the timer runs ous 
+//total wins and loses show on screen
+
+//when letter is clicked, letter is captured as a key event
+//when letter matches blank, blank should be replaced with letter.
+//when game is over, message should appear and timer should end
+//when user clicks start, timer should reset
+//when user refreshes, win and loss counts should persists. 
+
+
+function beginGame() {
+  let startButton = document.createElement("button");
+  startButton.textContent = "Start"
+  document.body.appendChild(startButton);
+}
+
+var resetButton = document.querySelector(".reset-button")
+
+function resetGame() {
+  winCount = 0;
+  lossCount = 0;
+
+  gamesWon()
+  gamesLost()
+}
+resetButton.addEventListener("click", resetGame)
